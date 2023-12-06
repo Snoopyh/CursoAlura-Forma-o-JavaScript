7385b477261bbe5da1d4a6962ddf744d9fd9dc74
Snoopyh <ismaelxd500@gmail.com>
2023-12-06 11:06:17 -0300

    last changes robotron 2000

---
+++ robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#robotron').addEventListener("click" , (event) => {
+  console.log(event)
+})
+function digaOi(nome){
+  console.log('oi ' + nome)
+  console.log('bem vindo ao robotron 2000')
+}
+digaOi("pedro")
